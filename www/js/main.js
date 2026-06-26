e3d69eb6e1fad2e24b1b4ef67276d52a5e83dc5b
Ahiezer Day P. Dalangin <ahiezerdayldalangin@gmail.com>
2026-06-26 13:56:14 +0800

    first commit

---
+++ www/js/main.js
@@ -0,0 +1,38 @@
+﻿const navbar = document.getElementById('navbar');
+const logo = document.querySelector('.logoText');
+
+window.addEventListener('scroll', () => {
+
+    const navLinks = document.querySelectorAll('#navbar a');
+
+    if (window.scrollY > 50) {
+
+        navbar.classList.add('bg-white', 'shadow-lg');
+
+        navLinks.forEach(link => {
+            link.classList.remove('text-white');
+            link.classList.add('text-gray-800');
+        });
+
+        if (logo) {
+            logo.classList.remove('text-white');
+            logo.classList.add('text-gray-800');
+        }
+
+    } else {
+
+        navbar.classList.remove('bg-white', 'shadow-lg');
+
+        navLinks.forEach(link => {
+            link.classList.remove('text-gray-800');
+            link.classList.add('text-white');
+        });
+
+        if (logo) {
+            logo.classList.remove('text-gray-800');
+            logo.classList.add('text-white');
+        }
+
+    }
+
+});
