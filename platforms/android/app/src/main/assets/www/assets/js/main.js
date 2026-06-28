a60444b1d2d171cbae7b87b90d8825118a823c7b
chyoan <christiandalayoan5@gmail.com>
2026-06-28 11:52:04 +0800

    cordova

---
+++ platforms/android/app/src/main/assets/www/assets/js/main.js
@@ -0,0 +1,47 @@
+﻿const navbar =
+document.getElementById("navbar");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 50){
+
+        navbar.classList.add(
+            "bg-white",
+            "shadow-lg"
+        );
+
+        navbar.classList.remove(
+            "bg-transparent"
+        );
+
+        document.querySelectorAll("#navbar a")
+        .forEach(link => {
+
+            link.classList.remove("text-white");
+            link.classList.add("text-gray-800");
+
+        });
+
+    }
+    else{
+
+        navbar.classList.remove(
+            "bg-white",
+            "shadow-lg"
+        );
+
+        navbar.classList.add(
+            "bg-transparent"
+        );
+
+        document.querySelectorAll("#navbar a")
+        .forEach(link => {
+
+            link.classList.remove("text-gray-800");
+            link.classList.add("text-white");
+
+        });
+
+    }
+
+});
